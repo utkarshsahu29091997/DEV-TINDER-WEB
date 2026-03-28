f6aeca6b8d84725fbfb91d96df6c08a64df471f6
Utkarsh Sahu <utkarshsahugithub101@gmail.com>
2026-03-29 03:55:45 +0530

    signup

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -31,7 +31,7 @@ const Feed = () => {
 
   return (
     feed && (
-      <div className=" flex justify-center  justify-center my-5">
+      <div className=" flex justify-center my-5">
         <UserCard user={feed[0]} />
       </div>
     )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,9 +6,12 @@ import { addUser } from "../utils/userSlice";
 import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-  const [email, setEmail] = useState("akash@gmail.com");
-  const [password, setPassword] = useState("Akash@123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
+  const [showLogin, setShowLogin] = useState(true);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -31,10 +34,54 @@ const Login = () => {
     }
   };
 
+  const handleSignUp = async () => {
+    try {
+      const response = await axios.post(
+        BASE_URL + "/signup",
+        {
+          email,
+          password,
+          firstName,
+          lastName,
+        },
+        {
+          withCredentials: true,
+        },
+      );
+      dispatch(addUser(response?.data?.data));
+      navigate("/");
+    } catch (error) {
+      setErrorMsg(error?.response?.data || "Something went wrong");
+    }
+  };
+
   return (
     <div className="flex justify-center my-10">
       <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-        <legend className="fieldset-legend">Login</legend>
+        <legend className="fieldset-legend">
+          {showLogin ? "Login" : "Signup"}
+        </legend>
+
+        {!showLogin && (
+          <>
+            <label className="label">Firstname</label>
+            <input
+              type="text"
+              className="input"
+              placeholder="Firstname"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <label className="label">Email</label>
+            <input
+              type="text"
+              className="input"
+              placeholder="Lastname"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </>
+        )}
 
         <label className="label">Email</label>
         <input
@@ -44,7 +91,6 @@ const Login = () => {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-
         <label className="label">Password</label>
         <input
           type="password"
@@ -55,9 +101,34 @@ const Login = () => {
         />
 
         <span className="text-red-300">{errorMsg}</span>
-        <button className="btn btn-neutral mt-4" onClick={handleLogin}>
-          Login
+        <button
+          className="btn btn-neutral mt-4"
+          onClick={showLogin ? handleLogin : handleSignUp}
+        >
+          {showLogin ? "Login" : "Signup"}
         </button>
+
+        {showLogin ? (
+          <span>
+            New user, Click on{" "}
+            <span
+              className="text-base font-medium cursor-pointer"
+              onClick={() => setShowLogin(false)}
+            >
+              Signup
+            </span>
+          </span>
+        ) : (
+          <span>
+            Already a user, Click on{" "}
+            <span
+              className="text-base font-medium cursor-pointer"
+              onClick={() => setShowLogin(true)}
+            >
+              Login
+            </span>
+          </span>
+        )}
       </fieldset>
     </div>
   );
